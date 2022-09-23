efcf61753a27324a5bfc0ebbcb6b2f8e8d8cbc77
DimaParachute <dimaparachute11@icloud.com>
2022-09-23 09:32:11 +0300

    Initial Commit

---
+++ NoteWizard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoteWizard
+//
+//  Created by Дмитрий Фетюхин on 23.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
